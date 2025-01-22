509c4f96a86f7e052777bf33e209be4ccac827cb
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-22 17:29:17 +0530

    Create 2890.py

---
+++ 2890.py
@@ -0,0 +1,8 @@
+#2890. Reshape Data: Melt
+
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return pd.melt(report, id_vars=["product"], value_vars=report.columns[1:], var_name="quarter", value_name="sales")
+    
+
